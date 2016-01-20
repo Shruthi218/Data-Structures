f67dc0a4889f108da623a320ee68c938f32c289e
Shruthi <shruthiravindran91@gmail.com>
2016-01-20 12:35:41 -0500

    latest commit

---
+++ Desktop/StringReverse.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class StringReverse
+{
+
+public static void main(String args[])
+{
+
+StringBuffer r = new StringBuffer("Java Programmins if fun");
+System.out.println(r.reverse());
+}
+}
