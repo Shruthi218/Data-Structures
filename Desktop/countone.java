ef51fa611458fe8475d6a7c0f2317dd509b01033
Shruthi <shruthiravindran91@gmail.com>
2016-01-21 12:30:00 -0500

    adding a file

---
+++ Desktop/countone.java
@@ -0,0 +1,24 @@
+class countone
+{
+
+public static void main(String args[])
+{
+int i;
+int arr[] = {1,1,0,0,0,0,0};
+
+for( i=0;i<arr.length;i++)
+{
+
+if(arr[i]==1)
+{
+i++;
+}
+else
+{
+
+}
+}
+System.out.println("number of one's are" +i);
+}
+}
+
